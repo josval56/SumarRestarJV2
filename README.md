76eae993eb74d34d9c77770159aba1a36724ec89
josval56 <josval56@gmail.com>
2020-11-06 20:15:39 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SumarRestarJV
+ 
---
+++ src/imparpar/Imparpar.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package imparpar;
+
+/**
+ *
+ * @author Valmore
+ */
+public class Imparpar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Formulario ventana = new Formulario();
+        ventana.setVisible(true);
+        Sumar_Restar ventana2 = new Sumar_Restar();
+        ventana2.setVisible(true);
+    }
+    
+}
